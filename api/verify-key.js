f93dd0c66ee8d728eb2db4a8835033b7da34cac5
Souvikpan220 <117498210+Souvikpan220@users.noreply.github.com>
2026-01-24 12:07:47 +0530

    Update verify-key.js

--- api/verify-key.js
+++ api/verify-key.js
@@ -3,13 +3,11 @@ import path from "path";
 
 export default function handler(req, res) {
   const { key } = req.body;
+  const data = JSON.parse(
+    fs.readFileSync(path.join(process.cwd(),"config","hotkeys.json"))
+  );
 
-  const filePath = path.join(process.cwd(), "config", "hotkeys.json");
-  const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
-
-  if (data.keys.includes(key)) {
-    return res.status(200).json({ success: true });
-  }
-
-  return res.status(401).json({ success: false });
+  data.keys.includes(key)
+    ? res.status(200).end()
+    : res.status(401).end();
 }
